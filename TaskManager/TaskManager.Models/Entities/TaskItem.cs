c34fba4e19c4a38ab952404d8d58cd07348ab638
Илья Фучаджи <twixtyz@gmial.com>
2026-04-08 20:57:32 +0300

    шаг 1: создание сущностей User и TaskItem в слое Models

---
+++ TaskManager/TaskManager.Models/Entities/TaskItem.cs
@@ -0,0 +1,19 @@
+﻿namespace TaskManager.Models.Entities;
+
+public class TaskItem
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public bool IsCompleted { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? Deadline { get; set; }
+
+    public int Priority { get; set; }
+
+    public int UserId { get; set; }
+    public User? User { get; set; }
+
+
+    public string? FilePath { get; set; }
+}

8e9aca031a9e2b55aa29e9a7a2a73ee41f183013
Илья Фучаджи <twixtyz@gmial.com>
2026-04-08 21:39:46 +0300

    создал сервис для запросов к api

---
+++ TaskManager/TaskManager.Desktop/Services/ApiService.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using Newtonsoft.Json;
+
+namespace TaskManager.Desktop.Services;
+
+public class ApiService
+{
+    private readonly HttpClient _httpClient;
+    
+    private const string BaseUrl = "http://localhost:5162/api/";
+
+    public ApiService()
+    {
+        _httpClient = new HttpClient();
+    }
+
+    public async Task<string?> LoginAsync(string email, string password)
+    {
+        var loginData = new { Email = email, Password = password };
+        var response = await _httpClient.PostAsJsonAsync($"{BaseUrl}Auth/login", loginData);
+
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<dynamic>(content);
+            return result?.token;
+        }
+        return null;
+    }
+}

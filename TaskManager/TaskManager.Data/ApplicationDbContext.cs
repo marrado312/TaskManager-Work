ffa64c91a622036caf96471fe47b138aa30bbb31
Илья Фучаджи <twixtyz@gmial.com>
2026-04-08 21:14:30 +0300

    шаг 2: база данных успешно развернута, таблицы созданы

--- TaskManager/TaskManager.Data/ApplicationDbContext.cs
+++ TaskManager/TaskManager.Data/ApplicationDbContext.cs
@@ -5,13 +5,17 @@ namespace TaskManager.Data;
 
 public class ApplicationDbContext : DbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
 
     public DbSet<User> Users => Set<User>();
     public DbSet<TaskItem> Tasks => Set<TaskItem>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+
         modelBuilder.Entity<TaskItem>()
             .HasOne(t => t.User)
             .WithMany()
